6f00c65b90d503384cef4be0d0b36a372e68da54
Lenovo <Lenovo@DESKTOP-U0L93D6>
2024-03-08 09:25:35 -0600

    Agregar archivos de proyecto.

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int nunTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = nunTimes;
+            return View();
+        
+        }
+        //public string Index() 
+        //{
+        //    return "This is my default action...";
+        //}
+
+        ////Get: /HelloWorld/Welcome/
+        ////Requieres using System.Text.Encodings.Web
+        //public string Welcome(string name, int numTimes = 1) 
+        //{
+        //    //return "This is the welcome action method";
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+
+        //}
+    }
+}
